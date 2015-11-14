88954a4cf1662bada611bdc44ffca6add451b3e6
unknown <you@example.com>
2015-11-13 19:18:08 -0500

    created start button and added timer

---
+++ js/game.js
@@ -0,0 +1,38 @@
+// Click Game Start
+function display( notifier, str )
+{
+    document.getElementById( notifier ).innerHTML = str;
+}
+
+function toMinuteAndSecond( x )
+{
+    return Math.floor( x / 60 ) + ':' +  ( x % 60 );
+}
+
+function setTimer( remain, actions )
+{
+    ( function countdown()
+    {
+        display( 'countdown', toMinuteAndSecond( remain ) );
+        actions[remain] && actions[remain]();
+        (remain -= 1) >= 0 && setTimeout( arguments.callee, 1000 );
+    })();
+}
+
+document.getElementById( 'buttonn' ).onclick = function()
+{
+    setTimer( 20, {
+        20: function()
+        {
+            display( 'notifier', 'seconds');
+        },
+        1: function()
+        {
+            display( 'notifier', 'second');
+        },
+        0: function()
+        { 
+            display( 'notifier', 'seconds');
+        }
+    } );
+}
