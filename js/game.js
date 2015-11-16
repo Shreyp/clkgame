43dbd7cf2d58a87b13d393744fcacf1428c9f84d
unknown <you@example.com>
2015-11-15 22:07:55 -0500

    push

--- js/game.js
+++ js/game.js
@@ -1,21 +1,7 @@
 // Click Game Start
-var countdown;
-var countdown_number;
+var points = 0
 
-function countdown_init() {
-    countdown_number = 11;
-    countdown_trigger();
-}
-
-function countdown_trigger() {
-    if(countdown_number > 0) {
-        countdown_number--;
-        document.getElementById('countdown_text').innerHTML = countdown_number;
-        if(countdown_number > 0) {
-            countdown = setTimeout('countdown_trigger()', 1000);
-        }
-}
-
-function countdown_clear() {
-    clearTimeout(countdown);
+function countPoints () {
+  points +=1;
+  console.log(points)
 }
