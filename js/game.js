c76f2af85d91670951d687a4845ea885946f783b
unknown <you@example.com>
2015-11-15 19:02:47 -0500

    added count funciton and timer

--- js/game.js
+++ js/game.js
@@ -1,1 +1,19 @@
 // Click Game Start
+var secondsCount = 0;
+var timerInterval;
+
+setTimeout(function() {
+    if(secondsCount === 0) {
+        clearInterval(timerInterval);
+        buttonn.parentNode.removeChild(buttonn);
+        alert("Your score is " + count);
+    }
+}, 10000);
+
+var count = 1
+
+function countPoints() {
+ count += 1;
+ console.log(countPoints)
+}
+
