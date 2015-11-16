cd2df0dcb08f921e4c5e365fc155931a41d09a26
unknown <you@example.com>
2015-11-16 15:14:52 -0500

    game starts on click

--- js/game.js
+++ js/game.js
@@ -1,22 +1,31 @@
-// Click Game Start
+var timerDecrease;
 var points = 0;
-var imageElements = document.getElementsByTagName("img");
-var secondsCount = 0;
 var toggleBtn = document.getElementById("toggleBtn");
+var secondsRemaining = 20;
+var clickImages = document.getElementsByTagName("img");
 
-function startGame() {
-  setTimeout(function() {
-    if(secondsCount === 0) {
-    alert("Your score is " + points);
-  }
-}, 20000);
-    for(var i = 0; i < imageElements.length; i++) {
-    imageElements[i].addEventListener("click", function() {
-    points +=1
-    console.log(points)})
-}
-}
-
-
+//Function to Start Timer
+  function startGame() {
+    timerDecrease = setInterval(timerCountdown, 1000);
+    };
 
 toggleBtn.addEventListener("click", startGame);
+
+//Function to Countdown
+function timerCountdown(){
+  if(secondsRemaining > 0){
+    document.getElementById("status").innerHTML = secondsRemaining-=1;
+  } else {
+    alert("times up your score is " + points)
+    clearInterval(timerDecrease);
+  }
+}
+
+//Function to count dem points
+  function countClicks(){
+    points +=1;
+}
+
+//Loop to add clicks to images
+for(var i = 0; i < clickImages.length; i++) {
+  clickImages[i].addEventListener("click", countClicks)}
