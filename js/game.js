df831e9d2260b34088dc4b14ba1716b6d18a6245
shreyp <shreyansap@gmail.com>
2016-04-09 17:53:17 -0400

    made game more functional

--- js/game.js
+++ js/game.js
@@ -1,31 +1,51 @@
+var toggleBtn = document.getElementById("toggleBtn");
+var clickImages = document.getElementsByTagName("img");
+var youScore = document.getElementById("#yourScore");
+var reset = document.getElementById("resetBtn");
+var statusBtn = document.getElementById("status");
+var secondsRemaining = 20;
 var timerDecrease;
 var points = 0;
-var toggleBtn = document.getElementById("toggleBtn");
-var secondsRemaining = 20;
-var clickImages = document.getElementsByTagName("img");
 
 //Start Game
 function startGame() {
   timerDecrease = setInterval(timerCountdown, 1000);
   for(var i = 0; i < clickImages.length; i++) {
-  clickImages[i].addEventListener("click", countClicks)};
+    clickImages[i].addEventListener("click", countClicks)
+  };
+  toggleBtn.setAttribute("class","btn btn-secondary");
+  resetBtn.setAttribute("disabled", "");
 };
+
+function resetGame(){
+  location.reload();
+}
+
+resetBtn.addEventListener("click", resetGame);
 
 toggleBtn.addEventListener("click", startGame);
 
 //Timer Ticks
 function timerCountdown(){
   if(secondsRemaining > 0){
-    document.getElementById("status").innerHTML = secondsRemaining-=1;
+    statusBtn.innerHTML = secondsRemaining-=1;
   } else {
-    alert("times up your score is " + points)
+    yourScore.innerHTML = points;
+    $('#myModal').modal({
+      backdrop:'static',
+      keyboard:false
+    });
+    resetBtn.removeAttribute("disabled", "");
     clearInterval(timerDecrease);
+    secondsRemaining = 20;
+    points=0;
   }
 }
 
 //Count Points
-  function countClicks(){
-    points +=1;
-    this.removeEventListener("click", countClicks);
-    console.log(points);
+function countClicks(){
+  points +=1;
+  this.src = "./images/x.png";
+  this.removeEventListener("click", countClicks);
+  console.log(points);
 }
